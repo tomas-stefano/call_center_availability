da4fbf137477a8e6f467160c82a74d691d939d85
Tomas D'Stefano <tomas_stefano@successoft.com>
2019-12-03 21:51:56 +0000

    Add rdoc comments and some final thoughts

--- lib/call_center_availability.rb
+++ lib/call_center_availability.rb
@@ -2,8 +2,43 @@ require 'call_center_availability/version'
 require 'active_support/core_ext'
 require 'business_time'
 
+## I was in doubt if I should change to CallCenter but both choices
+#  sounded right.
+#
+# CallCenterAvailability.
+#
+# Checks for the call center availability
+#
+# @example
+#
+#   ## e.g case now is 04th December 2019 09:00
+#   invalid_times = [
+#     Time.now + 1.hour,
+#     Time.now + 7.days,
+#     Time.new(2019, 12, 8, 13),
+#     Time.new(2019, 12, 7, 23)
+#   ]
+#
+#   ## e.g now is 04th December 2019 09:00
+#   valid_times = [
+#     Time.new(2019, 12, 6, 11),
+#     Time.new(2019, 12, 7, 11)
+#   ]
+#
+#   invalid_times.each do |invalid_time|
+#     call_center = CallCenterAvailability.new(invalid_time)
+#     puts "#{invalid_time} - #{call_center.available?}"
+#   end
+#
+#   valid_times.each do |valid_time|
+#     call_center = CallCenterAvailability.new(valid_time)
+#     puts "#{valid_time} - #{call_center.available?}"
+#   end
+#
 class CallCenterAvailability
   ## Sunday to Saturday
+  ## Maybe I should refactor the days that has same values but I
+  ## feel that would be an unecessary optimization.
   ##
   OPERATING_HOURS = [
     ['00:00', '00:00'],
@@ -15,12 +50,19 @@ class CallCenterAvailability
     ['09:00', '12:30'],
   ].freeze
 
+  # @param [Time] time Receives time of the pre booked call
+  #
   def initialize(time)
     @time = time
     @hour = time.strftime('%H:%M')
     @weekday = time.wday
   end
 
+  ## @return [true] if call center has availability.
+  ##
+  ## @return [false] if out of hours, earlier than 2 hours in
+  #   the future and more than 6 working days in the future.
+  ##
   def available?
     operating_hours? &&
       more_than_two_hours_in_the_future? &&
